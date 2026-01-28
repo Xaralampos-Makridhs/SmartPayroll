b3ab2f82d7274e406cb1982a2addfee2de1273ac
Xaralampos Makridhs <makridhs.xaralampos@gmail.com>
2026-01-28 14:16:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from emailservice.emailservice import EmailService
 
 mailer = EmailService()
 
-# Δοκιμαστική αποστολή στον εαυτό σου
+
 success = mailer.send_payslip(
     recipient_email="makridhs.xaralampos@gmail.com",
     subject="Test Payslip",
